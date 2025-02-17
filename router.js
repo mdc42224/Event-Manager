9d83bdee9aceb6da51ab08ab783eda5a8032dfa2
yitzchak <yitzchakrothman@gmail.com>
2025-02-17 15:53:30 +0200

    ammended router

--- router.js
+++ router.js
@@ -5,26 +5,7 @@ let currentUser;
 const singUpForm = document.getElementById("signupForm");
 const loginForm = document.getElementById("loginForm");
 
-const signUpInputs = singUpForm.querySelectorAll("input");
-
-
-
-document.getElementById("createAccount").addEventListener("click", () => {
-    loginForm.style.display = "none";
-    singUpForm.style.display = "block";
-    for (let i = 0; i < signUpInputs.length; i++) {
-        let inp = signUpInputs[i];
-        inp.addEventListener("input", () => {
-            inp.parentElement.querySelector(".error").style.display = "none";
-            V.validateInput(inp);
-        });
-    }
-})
-
-document.getElementById("logIn").addEventListener("click", () => {
-    singUpForm.style.display = "none";
-    loginForm.style.display = "block";
-})
+V.acitvateForms();
 
 
 singUpForm.addEventListener("submit", (e) => {currentUser=V.validateSignUp(e)
--- signUp.js
+++ signUp.js
@@ -1,4 +1,4 @@
-  
+
 
 
 const validator = {
@@ -11,89 +11,112 @@ const validator = {
 const singUpForm = document.getElementById("signupForm");
 const loginForm = document.getElementById("loginForm");
 
-   export function validateInput(inp) {
-        let userNameInp = singUpForm.querySelector("#createUser");
+export function validateInput(inp) {
+    let userNameInp = singUpForm.querySelector("#createUser");
 
-        userNameInp.parentElement.querySelector("#userFoundError").style.display = "none";
-        let property = validator[inp.name];
-        if (!property) {
-            inp.className = "error-border";
-            return false;
-        }
-        if (!property.test(inp.value)) {
-            inp.parentElement.querySelector(".error").style.display = "block";
-            inp.className = "error-border";
-            return false;
-        }
-        inp.className = "success-border";
-        return true;
+    userNameInp.parentElement.querySelector("#userFoundError").style.display = "none";
+    let property = validator[inp.name];
+    if (!property) {
+        inp.className = "error-border";
+        return false;
     }
+    if (!property.test(inp.value)) {
+        inp.parentElement.querySelector(".error").style.display = "block";
+        inp.className = "error-border";
+        return false;
+    }
+    inp.className = "success-border";
+    return true;
+}
 
-   export function validateSignUp(e) {
-        e.preventDefault();
-        let userNameInp = singUpForm.querySelector("#createUser");
+export function validateSignUp(e) {
+    e.preventDefault();
+    let userNameInp = singUpForm.querySelector("#createUser");
 
-        userNameInp.parentElement.querySelector("#userFoundError").style.display = "none";
+    userNameInp.parentElement.querySelector("#userFoundError").style.display = "none";
 
 
-        let users = (JSON.parse(localStorage.getItem("users"))) || [];
+    let users = (JSON.parse(localStorage.getItem("users"))) || [];
 
-        if (users.some(user => user.userName === userNameInp.value)) {
-            userNameInp.parentElement.querySelector("#userFoundError").style.display = "block";
-            return false;
+    if (users.some(user => user.userName === userNameInp.value)) {
+        userNameInp.parentElement.querySelector("#userFoundError").style.display = "block";
+        return false;
+    }
+    const inputs = singUpForm.querySelectorAll("input");
+
+
+    let validated = true;
+    for (let i = 0; i < inputs.length; i++) {
+        if (!validateInput(inputs[i])) {
+            validated = false;
         }
-        const inputs = singUpForm.querySelectorAll("input");
-
-
-        let validated = true;
+    }
+    if (validated) {
+        const newUser = {};
         for (let i = 0; i < inputs.length; i++) {
-            if (!validateInput(inputs[i])) {
-                validated = false;
-            }
+            let input = inputs[i];
+            newUser[input.name] = input.value;
         }
-        if (validated) {
-            const newUser = {};
-            for (let i = 0; i < inputs.length; i++) {
-                let input = inputs[i];
-                newUser[input.name] = input.value;
-            }
-            users.push(newUser);
-            localStorage.setItem("users", JSON.stringify(users));
-            clearForm();
-            return newUser;
-           
-        }
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+        clearForm();
+        return newUser;
+
+    }
+    return false;
+
+}
+
+export function validateLogin(e) {
+    let currentUser;
+    e.preventDefault();
+    let validated = true;
+    const userName = loginForm.querySelector("#userLogin").value;
+    const password = loginForm.querySelector("#passwordLogin").value;
+    const users = JSON.parse(localStorage.getItem("users"));
+    if (!users) validated = false;
+    else {
+        currentUser = users.find(user => user.userName === userName && user.password === password);
+    }
+    if (!validated || !currentUser) {
+        loginForm.querySelector("#passwordLoginError").style.display = "block";
         return false;
 
     }
-
-   export function validateLogin(e) {
-        let currentUser;
-        e.preventDefault();
-        let validated = true;
-        const userName = loginForm.querySelector("#userLogin").value;
-        const password = loginForm.querySelector("#passwordLogin").value;
-        const users = JSON.parse(localStorage.getItem("users"));
-        if (!users) validated = false;
-        else {
-            currentUser = users.find(user => user.userName === userName && user.password === password);
-        }
-        if (!validated || !currentUser) {
-            loginForm.querySelector("#passwordLoginError").style.display = "block";
-            return false;
-            
-        }
-        else{
-            clearForm();
-            return currentUser;
-        }
+    else {
+        clearForm();
+        return currentUser;
     }
-
-    function clearForm(){
-        singUpForm.reset();
-        loginForm.reset();
-    }
+}
+export function acitvateForms() {
+    const signUpInputs = singUpForm.querySelectorAll("input");
 
 
- 
+
+    document.getElementById("createAccount").addEventListener("click", () => {
+        loginForm.style.display = "none";
+        singUpForm.style.display = "block";
+        for (let i = 0; i < signUpInputs.length; i++) {
+            let inp = signUpInputs[i];
+            inp.addEventListener("input", () => {
+                inp.parentElement.querySelector(".error").style.display = "none";
+                validateInput(inp);
+            });
+        }
+    })
+
+    document.getElementById("logIn").addEventListener("click", () => {
+        singUpForm.style.display = "none";
+        loginForm.style.display = "block";
+    })
+
+}
+
+function clearForm() {
+    singUpForm.reset();
+    loginForm.reset();
+}
+
+
+
 
