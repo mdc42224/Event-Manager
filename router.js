38ceffbc25c2e36de2d2888ddd8d6b472e8d3406
Mordechai <mdc42224@gmail.com>
2025-02-20 13:28:43 +0200

    test

--- router.js
+++ router.js
@@ -154,8 +154,8 @@ function submitNewForm(e){
     let guestsGroups = eventForm.querySelectorAll('.guestContainer')
     let guests =[];
     guestsGroups.forEach((guestGroup)=>{
-        let guestName = guestGroup.querySelector("guestName.").value;
-        let guestEmail = guestGroup.querySelector('guestEmail').value;
+        let guestName = guestGroup.querySelector("#guestName").value;
+        let guestEmail = guestGroup.querySelector('#guestEmail').value;
         guests.push( {"guestName":guestName,email:guestEmail})
     })
     let formInfo = {name:eventName,location:eventLocation,date:eventDate,guestList:guests,userName:store.getState.user};
